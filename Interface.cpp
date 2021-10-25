d6d3389bfd0fe6de3f2c66ef48aca4b9b6c7b5bf
inkustr <63226634+inkustr@users.noreply.github.com>
2021-10-25 23:41:50 +0300

    Update Interface.cpp

--- Interface.cpp
+++ Interface.cpp
@@ -13,22 +13,25 @@ struct Graph {
 Graph adjacency_matrix_initialization();
 
 // умножение матриц
-std::vector<std::vector<int64_t>> matrix_multiplication(const std::vector<std::vector<int64_t>>& matrix_first, const std::vector<std::vector<int64_t>>& matrix_second);
+std::vector<std::vector<int64_t>> matrix_multiplication(
+    const std::vector<std::vector<int64_t>>& matrix_first,
+    const std::vector<std::vector<int64_t>>& matrix_second);
 
-// бинарное возведение матрицы в степень
-std::vector<std::vector<int64_t>> matrix_binary_exponentiation(const std::vector<std::vector<int64_t>>& matrix, int power);
+// возведение матрицы в степень
+std::vector<std::vector<int64_t>> matrix_exponentiation(
+    const std::vector<std::vector<int64_t>>& matrix, int power);
 
-// подсчёт матрицы, в которой хранится количество путей нужной длины из первой вершины
+// подсчёт матрицы, в которой хранится количество путей нужной длины
 int64_t calculate_amount_of_fixed_routes(const Graph& labyrinth);
 
 // вывод количества путей фиксированной длины из первой вершины
-void write(int amount_of_fixed_routes);
+void write(int64_t amount_of_fixed_routes);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     const Graph labyrinth = adjacency_matrix_initialization();
-    const int64_t amount_of_fixed_routes = calculate_amount_of_fixed_routes(labyrinth);
+    const int64_t amount_of_fixed_routes =
+        calculate_amount_of_fixed_routes(labyrinth);
     write(amount_of_fixed_routes);
     return 0;
 }
-
