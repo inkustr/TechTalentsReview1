b699fe06a7069240240c3c4d690fa94bc28c55f9
inkustr <63226634+inkustr@users.noreply.github.com>
2021-10-25 15:33:31 +0300

    Update Interface.cpp

--- Interface.cpp
+++ Interface.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <vector>
 
-struct graph {
+struct Graph {
     int vertex_amount;
     int edge_amount;
     int required_power;
@@ -10,22 +10,24 @@ struct graph {
 };
 
 // инициализация матрицы смежности
-graph init_adjacency_matrix();
+Graph init_adjacency_matrix();
 
 // умножение матриц
-std::vector<std::vector<int64_t>> matrix_multiplication(const std::vector<std::vector<int64_t>>& matrix_first, const std::vector<std::vector<int64_t>>& matrix_second, int size);
+std::vector<std::vector<int64_t>> matrix_multiplication(const std::vector<std::vector<int64_t>>& matrix_first, const std::vector<std::vector<int64_t>>& matrix_second);
 
 // бинарное возведение матрицы в степень
-std::vector<std::vector<int64_t>> matrix_binary_exponentiation(const std::vector<std::vector<int64_t>>& matrix, int size, int power);
+std::vector<std::vector<int64_t>> matrix_binary_exponentiation(const std::vector<std::vector<int64_t>>& matrix, int power);
+
+// подсчёт матрицы, в которой хранится количество путей нужной длины
+Graph find_answer_graph(const Graph& labyrinth);
 
 // восстановление и вывод ответа
-void write_fixed_routes_from_vertex(const graph& answer_graph);
+void write_fixed_routes_from_vertex(const Graph& answer_graph);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
-    const graph labyrinth = init_adjacency_matrix();
-    const graph answer_graph = { labyrinth.vertex_amount, labyrinth.edge_amount, 0, matrix_binary_exponentiation(labyrinth.adjacency_matrix, labyrinth.vertex_amount, labyrinth.required_power) };
+    const Graph labyrinth = init_adjacency_matrix();
+    const Graph answer_graph = find_answer_graph(labyrinth);
     write_fixed_routes_from_vertex(answer_graph);
     return 0;
 }
-
